ef58d6b61e74f443e25d0fd2476ba05f72a08869
Jose Angel Espinosa Bautista <joseespinosa@fb.com>
2022-06-20 15:51:44 -0700

    passed all

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import * as React from "react"
+import { useState } from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
+import Header from "./components/Header/Header"
+import Instructions from "./components/Instructions/Instructions"
+import Chip from "./components/Chip/Chip"
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel"
 
 // don't move this!
 export const appInfo = {
@@ -21,6 +26,43 @@ export const appInfo = {
 const { data, categories, restaurants } = createDataSet()
 
 export function App() {
+
+  const [clickedCategory, setClickedCategory] = useState();
+  const [clickedRestaurant, setClickedRestaurant] = useState();
+  const [clickedItem, setClickedItem] = useState();
+
+
+
+
+
+  let selectedMenu = data.filter((item) => {
+      return (item.restaurant === clickedRestaurant && item.food_category === clickedCategory)
+    });
+
+
+
+
+
+
+  function handleCategoryClick(label) {
+    setClickedCategory(label);
+
+  }
+  function handleRestaurantClick(label) {
+    setClickedRestaurant(label);
+
+  }
+
+  function handleItemClick(label) {
+    let item = data.find(item => item.item_name === label);
+    //console.log('iteeee', item.item_name, 'labeeel', label);
+    setClickedItem(item);
+  }
+
+
+
+
+
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
@@ -28,30 +70,48 @@ export function App() {
         <div className="categories options">
           <h2 className="title">Categories</h2>
           {/* YOUR CODE HERE */}
+          {categories.map(category => {
+            const active = (clickedCategory === category) ? true : false;
+            return (<Chip key={category} label={category} isActive={active} handleClick={handleCategoryClick}>{category}</Chip>);
+          })}
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
         {/* HEADER GOES HERE */}
-
+        <Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description} />
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">
+
+            {/* YOUR CODE HERE */}
+            {restaurants.map(restaurant => {
+              const active = (clickedRestaurant === restaurant) ? true : false;
+              return (<Chip key={restaurant} label={restaurant} isActive={active} handleClick={handleRestaurantClick}>{restaurant}</Chip>);
+            })}
+          </div>
         </div>
 
         {/* INSTRUCTIONS GO HERE */}
-
+        <Instructions instructions={appInfo.instructions.start}></Instructions>
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
             {/* YOUR CODE HERE */}
+            {selectedMenu.map((item) => {
+              const active = (clickedItem === item) ? true : false;
+              return (<Chip key={item.item_name} isActive={active} label={item.item_name} handleClick={handleItemClick}></Chip>)
+            })}
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">
+            {/* YOUR CODE HERE */}
+            {clickedItem && <NutritionalLabel item={clickedItem}></NutritionalLabel>}
+          </div>
         </div>
 
         <div className="data-sources">
--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,10 +1,14 @@
 import * as React from "react"
 import "./Chip.css"
 
-export function Chip({ label = "", isActive = false }) {
+export function Chip({ label = "", isActive = false, handleClick}) {
+
+  let buttonClassName = isActive? "chip active" : "chip";
+  //console.log('the cunfion', handleClick)
+
   return (
-    <button className="chip">
-      <p className="label"></p>
+    <button className={buttonClassName} onClick = {()=>handleClick(label)} >
+      <p className="label">{label}</p>
       <span className="close" role="button">{`X`}</span>
     </button>
   )
